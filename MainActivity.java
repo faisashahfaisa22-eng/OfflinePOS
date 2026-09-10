072505c6f23af225753f479ffadaf2378cc82e6c
faisashahfaisa22-eng <faisashahfaisa22@gmail.com>
2026-09-10 20:32:31 +0430

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -19,7 +19,7 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-android.webkit.DownloadListener;
+import android.webkit.DownloadListener;
 import android.widget.Toast;
 
 

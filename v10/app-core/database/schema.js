e802ba82369397ef9a448cd0e897705c06cf3272
faisashahfaisa22-eng <faisashahfaisa22@gmail.com>
2026-09-14 21:18:19 +0430

    Add v10 database schema foundation

---
+++ v10/app-core/database/schema.js
@@ -0,0 +1,15 @@
+// POS & Accounts Pro v10 Database Schema Foundation
+
+const Tables = {
+  products: [],
+  sales: [],
+  purchases: [],
+  expenses: [],
+  customers: [],
+  suppliers: [],
+  transactions: [],
+  stockMovements: [],
+  ledger: []
+};
+
+export default Tables;
